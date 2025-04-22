784864d9c5d2a018b1112c99aad12ee0fdf3cfb8
Student <azaryanmail@gmail.com>
2025-04-22 18:03:43 +0300

    фиксил файла в gitIgnore

---
+++ src/utils/config.js
@@ -0,0 +1,11 @@
+export const FIREBASE_CONFIG = {
+   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+};
+
+export const IMGBB_API_KEY = import.meta.env.VITE_IMGBB_API_KEY;
